1ba0874ce3f42b259e104c986572dd1a32806c59
Gareth Owen <gareth.w.owen@gmail.com>
2015-09-08 18:25:14 +0100

    Added order/order details classes and checkout pages

---
+++ WingtipToys/Models/Order.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace WingtipToys.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public System.DateTime OrderDate { get; set; }
+
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [DisplayName("First Name")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        [DisplayName("Last Name")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [StringLength(40)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        [DisplayName("Postal code")]
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [DisplayName("Email address")]
+        [RegularExpression(@"[A-za-z0.9. %+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string PaymentTransactionId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool HasBeenShipped { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
